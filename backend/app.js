ddd9e66df545c44a882ab7693e5b499fe1ce176e
danpinon <danielpformoso@gmail.com>
2021-06-10 18:51:05 -0500

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app =  express()
+
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const dotenv = require('dotenv')
+const path = require('path')
+
+
+const errorMiddleware = require('./middlewares/errors')
+
+//setting up config file
+if(process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').dotenv.config({path: 'backend/config/'})
+dotenv.config({ path: 'config/config.env'})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(fileUpload())
+
+
+
+
+//IMPORT ALL ROUTES
+const products = require('./routes/product')
+const auth = require('./routes/auth')
+const order = require('./routes/order')
+
+app.use('/api/v1', products)
+app.use('/api/v1', auth)
+app.use('/api/v1', order)
+
+if(process.env.NODE_ENV === 'PRODUCTION') {
+   app.use(express.static(path.join(__dirname, '../frontend/build')))
+   app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+   })
+}
+
+//Middleware to handle errors
+app.use(errorMiddleware)
+
+module.exports = app
